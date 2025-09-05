a23db85589d5e7710193518ccc291b3a2a2efbac
kella <f.rianjafimamonjisoa@esis-italia.com>
2025-09-05 08:12:33 +0200

    ✨ F1: Models and migrations

---
+++ app/Models/Travel.php
@@ -0,0 +1,58 @@
+<?php
+
+namespace App\Models;
+
+use Cviebrock\EloquentSluggable\Sluggable;
+use Illuminate\Database\Eloquent\Casts\Attribute;
+use Illuminate\Database\Eloquent\Concerns\HasUuids;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasMany;
+use Illuminate\Support\Str;
+
+class Travel extends Model
+{
+    use HasFactory, Sluggable, HasUuids;
+
+    protected $table = 'travels';
+    protected $keyType = 'string';
+    public $incrementing = false;
+
+    protected $fillable = [
+        'is_public',
+        'name',
+        'description',
+        'number_of_days',
+    ];
+
+
+    public function tours(): HasMany
+    {
+        return $this->hasMany(Tour::class);
+    }
+
+    protected static function booted()
+    {
+        static::creating(function ($model) {
+            if (!$model->{$model->getKeyName()}) {
+                $model->{$model->getKeyName()} = (string) Str::uuid();
+            }
+        });
+    }
+    public function sluggable(): array
+    {
+        return [
+            'slug' => [
+                'source' => 'name',
+                'unique' => true,
+                'onUpdate' => false,
+            ]
+        ];
+    }
+    public function numberOfNigths(): Attribute
+    {
+        return Attribute::make(
+            get: fn($value, $attributes) => $attributes['number_of_days'] - 1
+        );
+    }
+}
