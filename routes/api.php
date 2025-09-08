44498d28c9c1c646784699183c0888a979b259a0
kella <f.rianjafimamonjisoa@esis-italia.com>
2025-09-08 10:43:00 +0200

    ✨ F03: Admin to create Travel

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\Http\Controllers\Api\V1\Admin\TravelController as AdminTravelController;
+use App\Http\Controllers\Api\V1\Auth\LoginController;
 use App\Http\Controllers\Api\V1\TourController;
 use App\Http\Controllers\Api\V1\TravelController;
 use Illuminate\Http\Request;
@@ -9,3 +11,9 @@ use Illuminate\Support\Facades\Route;
 //Route::apiResource('v1/travels', TravelController::class);
 Route::get('v1/travels', [TravelController::class, 'index']);
 Route::get('v1/travels/{travel:slug}/tours', [TourController::class, 'index']);
+
+Route::prefix('v1/admin')->middleware(['auth:sanctum', 'role:admin'])->group(function () {
+    Route::post('travels', [AdminTravelController::class, 'store']);
+});
+
+Route::post('v1/login', LoginController::class);
